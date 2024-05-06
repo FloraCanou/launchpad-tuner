7e760b11c69cdc8158a717dd4f5925e5ad54c665
Flora Canou <floracanou@qq.com>
2024-05-06 22:36:45 +0800

    Lighting

--- lpt_main.py
+++ lpt_main.py
@@ -1,28 +1,94 @@
 # © 2024 Flora Canou, based on Godtone's work
 # This work is licensed under the AGPLv3 <https://www.gnu.org/licenses/agpl-3.0.html>
 
-import math
 from lpt_io import *
 
 # 0x00 -- 0x03 grayscale
 # 0x04 -- 0x3B 14 chromas * 4 levels
 # 0x3C -- 0x7F other colors
 
-rb7 = [0x05, 0x0d, 0x15, 0x1d, 0x25, 0x2d, 0x35] # rb for rainbow
-color_map = [0x00]*109
+rb = [0x09, 0x11, 0x19, 0x29, 0x31, 0x39] # rb for rainbow
 
-def mapping (x, y, n, base_note = 60):
+def tone (n):
+    """
+    Approximate number of steps of the whole tone in n-edo. 
+    Precision should be enough for n < 20,000.
+    """
+    return int (round (n*0.169925))
+
+def get_coordinates (k):
+    """Gets the coordinates (row, col) for key code k on the Launchpad."""
+    k %= 100
+    return k//10, k%10
+
+def wrapper (n):
+    """
+    (tone - step, step) isomorphic layout coloring and mapping function. 
+    In this layout, each note along the main diagonal line ascends by a whole tone. 
+    We're coloring these notes and all the duplicates thereof. 
+    Assume the base note is Db, we're coloring Db, Eb, F, G, A, B, C#, and D#. 
+    For plain-fifth edos we're also coloring D, E, F#, G#, Ab, Bb, C and D. 
+
+    low Db  = gray
+    low D   = red
+    Eb      = orange
+    E       = yellow
+    F       = chartreuse
+    F#      = green
+    G       = spring
+    G#      = cyan #1 (aquamarine)
+    Ab      = cyan #2 (capri)
+    A       = azure
+    Bb      = blue
+    B       = violet
+    C       = magenta
+    C#      = rose
+    high D  = red
+    high D# = gray
+    """
+    global mi_launchpad_name, mo_launchpad_name, mo_loopmidi_name
+    global rb
+    
+    # approximate number of steps of the whole tone
+    # and chroma for plain-fifth edos
+    t = tone (n)
+    chroma = (7*t - n)//2 if (n + t) % 2 == 0 else None
+
+    color_map = [0x00]*100
+    for k in range (0, 100):
+        row, col = get_coordinates (k)
+        if row == 0 or row == 9 or col == 0 or col == 9:
+            continue # skip control keys
+        
+        # light low Db (bottom left) and high D# (upper right)
+        if row == 1 and col == 1 or row == 8 and col == 8:
+            color_map[k] = 0x02
+        # light Eb, F, G, A, B, and C#
+        elif (row - col) % t == 0:
+            color_offset = (col + (row - col)//t - 2) % 6
+            color_map[k] = rb[color_offset]
+        elif chroma:
+            # light D, E, F#, G#
+            if ((row - col) % t % chroma == 0 and (row - col) % t // chroma == 1
+                    and col + (row - col)//t <= 4): # don't light A#
+                color_offset = (col + (row - col)//t - 2) % 6
+                color_map[k] = (rb[color_offset] + 4 - 4) % 0x38 + 4
+            # light Ab, Bb, C and D
+            elif ((col - row) % t % chroma == 0 and (col - row) % t // chroma == 1
+                    and col + (row - col)//t >= 4): # don't light Gb
+                color_offset = (col + (row - col)//t - 1) % 6
+                color_map[k] = (rb[color_offset] - 4 - 4) % 0x38 + 4
+        
+    mapping (n, x = t - 1, y = 1, color_map = color_map, base_note = n)
+
+def mapping (n, x, y, color_map = [0x00]*100, base_note = 60):
     """
     (x, y) isomorphic layout mapping function
     with register keys for each row that move the notes by n steps. 
+    color_map: a 100-entry (0 to 99) list of colors from Launchpad's color palette. 
     base_note: pad at bottom left.
     """
     global mi_launchpad_name, mo_launchpad_name, mo_loopmidi_name
-    
-    def get_coordinates (k):
-        """Gets the coordinates (row, col) for key code k on the Launchpad."""
-        k %= 100
-        return k//10, k%10
 
     def transform (midi_note):
         """
@@ -35,7 +101,7 @@ def mapping (x, y, n, base_note = 60):
     with mido.open_output (mo_launchpad_name) as color:
         print ("Coloring..")
         for k in range (1, 109):
-            msg = mido.Message ('note_on', channel = 0, note = k, velocity = color_map[k])
+            msg = mido.Message ('note_on', channel = 0, note = k, velocity = color_map[k % 100])
             color.send (msg)
 
     # transform the launchpad's midi input (mi) into custom midi output (mo)
