90150025f43c51b7702c2f64a4905f99a0213f01
Flora Canou <floracanou@qq.com>
2024-05-11 20:29:25 +0800

    Store control key states with a dictionary too

--- lpt_main.py
+++ lpt_main.py
@@ -111,7 +111,7 @@ def mapping (n, x, y, color_map = [0x00]*100, base_note = 60):
         """
         row, col = get_coordinates (midi_note)
         base_and_offset = base_note + col*x + row*y
-        reg = control_reg if row == control_row else 0
+        reg = n if control_state[row] else 0
         if (result := base_and_offset + reg) < 0x80:
             return result
         else:
@@ -125,10 +125,13 @@ def mapping (n, x, y, color_map = [0x00]*100, base_note = 60):
 
     # transform the launchpad's midi input (mi) into custom midi output (mo)
     with mido.open_input (mi_launchpad_name) as mi, mido.open_output (mo_loopmidi_name) as mo:
-        control_row, control_col, control_reg = 0, 0, 0
+        control_row, control_col = 0, 0
         note_state = dict () # for keeping track of equivalent notes being held
         for note in range (0, 0x80):
             note_state[note] = 0
+        control_state = dict () # for keeping track of register keys being held
+        for control_row in range (1, 9):
+            control_state[control_row] = 0
         for msg in mi: # process midi messages from launchpad
             msg_to_send = msg.copy ()
             if msg.type == "control_change":
@@ -140,8 +143,12 @@ def mapping (n, x, y, color_map = [0x00]*100, base_note = 60):
                     if note_state[note]:
                         mo.send (mido.Message ("note_on", note = note, velocity = 0))
                         note_state[note] -= 1
+                if msg.value:
+                    control_state[control_row] += 1
+                else:
+                    control_state[control_row] -= 1
+                # print (control_row, control_state[control_row], sep = ", ")
                 
-                control_reg = n if msg.value else 0
             if msg.type == "note_on":
                 note = transform (msg.note)
                 # turn off equivalent midi note before repressing
@@ -155,7 +162,7 @@ def mapping (n, x, y, color_map = [0x00]*100, base_note = 60):
 
                 msg_to_send = mido.Message ("note_on", note = note, velocity = msg.velocity)
 
+            mo.send (msg_to_send)
+
             if not msg.type == "clock" and not msg.type == "aftertouch":
                 print (msg_to_send)
-
-            mo.send (msg_to_send)
